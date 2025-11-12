645a6b91fe172bb648d6cbb3ebe8215f898a31db
bougs93 <happyreport@gmail.com>
2025-11-12 21:21:23 +0900

    fix: 결보강 문서 메뉴 변경.

--- lib/models/document_type.dart
+++ lib/models/document_type.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 /// 문서 타입 열거형
 enum DocumentType {
-  /// 결보강 계획서
+  /// 결보강 날짜선택
   substitutionPlan,
+  
+  /// 결보강 출력
+  fileExport,
   
   /// 학급안내
   classNotice,
   
   /// 교사안내
   teacherNotice,
-
-  /// 파일 출력
-  fileExport,
 }
 
 /// DocumentType 확장 메서드들
@@ -21,13 +21,14 @@ extension DocumentTypeExtension on DocumentType {
   String get displayName {
     switch (this) {
       case DocumentType.substitutionPlan:
-        return '결보강 계획서';
+        return '결보강 날짜선택';
+      case DocumentType.fileExport:
+        return '결보강 출력';
       case DocumentType.classNotice:
         return '학급안내';
       case DocumentType.teacherNotice:
         return '교사안내';
-      case DocumentType.fileExport:
-        return '파일 출력';
+  
     }
   }
   
